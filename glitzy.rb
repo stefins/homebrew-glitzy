8f4b16b5cf701859a6f9f68c4ecc03515d566d8f
Stef <sstefin@bk.ru>
2021-03-29 17:03:41 -0500

    v1.0.2

--- glitzy.rb
+++ glitzy.rb
@@ -1,8 +1,8 @@
 class Glitzy < Formula
   desc "A simple password manager written in Go"
   homepage "github.com/stefins/glitzy"
-  url "https://github.com/stefins/glitzy/releases/download/v1.0.1/glitzy-darwin-10.6-amd64"
-  sha256 "a88320b19020af15ea9fc63167931fe85eb5ad94a6cecb2d0a9ba76f865e015a"
+  url "https://github.com/stefins/glitzy/releases/download/v1.0.2/glitzy-darwin-10.6-amd64"
+  sha256 "56d855601d0dca7f2404f2f99cb0ef5df1d8c0a0e7d7d42231947f0b6b0c13ec"
   license "MIT"
 
   def install
